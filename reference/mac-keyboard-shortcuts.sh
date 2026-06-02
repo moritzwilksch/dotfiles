74e6d1dd07ad7af033e14a734c2c83fa5a2a1e59
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2026-06-02 13:54:41 +0200

    Add macOS keyboard shortcuts for PowerPoint

---
+++ reference/mac-keyboard-shortcuts.sh
@@ -0,0 +1,11 @@
+defaults delete com.microsoft.Powerpoint NSUserKeyEquivalents
+
+defaults write com.microsoft.Powerpoint NSUserKeyEquivalents -dict \
+"Align Left" "^~@h" \
+"Align Bottom" "^~@j" \
+"Align Top" "^~@k" \
+"Align Right" "^~@l" \
+"Align Center" "^~@c" \
+"Align Middle" "^~@m" \
+"Distribute Horizontally" "^~h" \
+"Distribute Vertically" "^~v"
